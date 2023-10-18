6985a303494ee023c0147d1b3307fae42a7d77a7
1gor.legit <89613269+Igor-Carbonari-Silva@users.noreply.github.com>
2023-10-18 16:33:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ var idade;
 var dataAtual;
 var listaParticipantesPalestrantes;
 
-nome = readlineSync.question("Digite seu nome: ");
-dataDoEvento = readlineSync.question("Informe a data do evento: ");
-idade = readlineSync.question("Digite sua idade: ");
-dataAtual = readlineSync.question("Informe a data atual: ");
-
 for (listaParticipantesPalestrantes = 0; listaParticipantesPalestrantes < 100; listaParticipantesPalestrantes++){
 
+    nome = readlineSync.question("Digite seu nome: ");
+    dataDoEvento = readlineSync.question("Informe a data do evento: ");
+    idade = readlineSync.question("Digite sua idade: ");
+    dataAtual = readlineSync.question("Informe a data atual: ");
+    
     if(dataDoEvento > dataAtual){
         //condição verdadeira
         if(idade > 18){
@@ -31,12 +31,6 @@ for (listaParticipantesPalestrantes = 0; listaParticipantesPalestrantes < 100; l
         //condição falsa
         console.log("Cadastro não permitido, data inválida!");
     }
-
-    nome = readlineSync.question("Digite seu nome: ");
-    dataDoEvento = readlineSync.question("Informe a data do evento: ");
-    idade = readlineSync.question("Digite sua idade: ");
-    dataAtual = readlineSync.question("Informe a data atual: ");
-    
 }
 
 console.log("Cadastro negado, lista de participantes excedida!");
